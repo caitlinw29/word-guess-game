4b2248e0cb1909faa013244fcbca0d3f78d1d4ae
Caitlin Wall <caitlinwall96@gmail.com>
2021-12-17 14:28:11 -0500

    Create header, and add base functionality to start button and instructions

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+var startBtn = document.getElementById("start");
+const overlay = document.getElementById("overlay");
+var instructions = document.getElementById("instructions");
+let overlayVisible = false;
+
+startBtn.addEventListener("click", startGame);
+
+function startGame(){
+  document.addEventListener("keydown", keydownAction);
+}
+
+
+//listen for keys pressed
+var keyPressed;
+
+function keydownAction(event) {
+  keyPressed = event.key.toUpperCase();
+  var stringLetters = "abcdefghijklmnopqrstuvwxyz";
+  var letters = stringLetters.split("");
+
+  if (!letters.includes(event.key)) {
+    return;
+  } else {
+    console.log(keyPressed);
+    checkLetter();
+  }
+}
+
+
+//check if letter is in current word
+function checkLetter(){
+
+}
+
+
+
+//functions to show how-to-play overlay on click of button
+function overlayOn() {
+  overlay.style.display = "block";
+  overlayVisible = true;
+}
+
+function overlayOff() {
+  overlay.style.display = "none";
+  overlayVisible = false;
+}
+overlay.addEventListener('click', overlayOff);
+startBtn.addEventListener('click', overlayOff);
+instructions.addEventListener('click', function(){
+  if (overlayVisible) {
+    overlayOff();
+  } else {
+    overlayOn();
+  }
+});
+
+
+
